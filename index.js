3eab8a57cf037bf4772dab2429cee037a46a6816
nolbol <nchristi@purdue.edu>
2017-06-27 11:21:13 -0400

    Added the name to a paragraph inside the stats div and added another form

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ function changeHeading(ev) {
     ev.preventDefault()
     const f = ev.target
     const name = f.personName.value
-    const heading = document.querySelector('h1')
-    heading.textContent = name
+    const stats = document.querySelector('#stats')
+    stats.innerHTML = name
 }
+
+
 
 const personForm = document.querySelector('#person-form')
 personForm.addEventListener('submit', changeHeading)
