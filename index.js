c7224c29eae8c43ba0d472e9a83631a2db6d0527
nolbol <nchristi@purdue.edu>
2017-06-27 15:46:35 -0400

    Add stats in an unordered list

--- index.js
+++ index.js
@@ -6,7 +6,19 @@ function changeHeading(ev) {
     const second = f.personSecond.value
     const color = f.favoritecolor.value
     f.style.color = color
-    //checking the font color to see if its valid
+    const list = document.createElement('ul')
+
+    const nameItem = document.createElement('li')
+    nameItem.textContent = `Name: ${name}`
+    list.appendChild(nameItem)
+
+    const ageItem = document.createElement('li')
+    ageItem.textContent = `Age:${age}`
+    list.appendChild(ageItem)
+
+    const colorItem = document.createElement('li')
+    colorItem.textContent = `Favorite Color: ${color}`
+    list.appendChild(colorItem)
 
     stats.innerHTML = name + "; second" + second
     //div.innerHTML = ${name}, second ${second}
