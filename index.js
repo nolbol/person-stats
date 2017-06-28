2e64ab161619c87ca5e3580f3ce9aa1125cc2892
nolbol <nchristi@purdue.edu>
2017-06-28 10:21:08 -0400

    Added the function 'Render Color'

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
+function renderColor (color) {
+    const colorDiv = document.createElement('div')
+    colorDiv.style.backgroundColor = color
+    colorDiv.style.width = '6rem'
+    colorDiv.style.height = '3rem'
+    return colorDiv
+}
+
+function renderListItem () {
+    
+}
+
 function changeHeading(ev) {
     ev.preventDefault()
     const f = ev.target
     const name = f.personName.value
-    const stats = document.querySelector('#stats')
-    const second = f.personSecond.value
-    const color = f.favoritecolor.value
-    f.style.color = color
+    const age = f.personSecond.value
+    const color = f.favoriteColor.value
+
+    const div = document.querySelector('#stats')
+
     const list = document.createElement('ul')
 
     const nameItem = document.createElement('li')
@@ -13,17 +26,23 @@ function changeHeading(ev) {
     list.appendChild(nameItem)
 
     const ageItem = document.createElement('li')
-    ageItem.textContent = `Age:${age}`
+    ageItem.textContent = `Age: ${age}`
     list.appendChild(ageItem)
 
     const colorItem = document.createElement('li')
-    colorItem.textContent = `Favorite Color: ${color}`
+    colorItem.textContent = 'Favorite Color: '
+
+    const colorDiv = renderColor(color)
+    colorItem.appendChild(colorDiv)
+    
     list.appendChild(colorItem)
 
-    stats.innerHTML = name + "; second" + second
-    //div.innerHTML = ${name}, second ${second}
-    
+    div.appendChild(list)
 }
+
+
+
+
 
 
 
